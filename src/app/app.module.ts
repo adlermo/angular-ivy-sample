5dfdc79c2ad9a3291e1eaf8abb58b1785f8514c1
adlermo <adlerorteney@gmail.com>
2022-02-24 20:04:36 -0300

    Creating first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   imports: [AppRoutingModule, BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [WeatherCardComponent],
   imports: [
     CommonModule
   ]
