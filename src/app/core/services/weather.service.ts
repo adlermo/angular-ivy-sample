0c5eb8372926667653ec2d4c89f88e2010a99501
adlermo <adlerorteney@gmail.com>
2022-02-24 19:40:59 -0300

    Creating weather service and interface

---
+++ src/app/core/services/weather.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { Current } from '../../shared/interfaces/current';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  apiURL =
+    'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=d21dd0e334529048170784d3f77b3f74&units=imperial';
+
+  constructor(private readonly http: HttpClient) {}
+
+  getCurrentWeather(): Observable<Current> {
+    return this.http.get<Current>(this.apiURL);
+  }
+}
