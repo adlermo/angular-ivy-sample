3dae24b76f0a704975ac7a0104c151dba5328505
adlermo <adlerorteney@gmail.com>
2022-02-24 19:58:10 -0300

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
