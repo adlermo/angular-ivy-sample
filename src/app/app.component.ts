8d50be2bd3af826bb2f54ba437470dd977ae1022
adlermo <adlerorteney@gmail.com>
2022-02-25 12:28:27 -0300

    Botão voltar baseado na rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  path: string;
+  action: string;
+
+  constructor(private readonly router: Router) {
+    router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) this.changeUrl(e.url);
+    });
+  }
+
+  changeUrl(url: string) {
+    switch (url) {
+      case '/current':
+        this.path = '';
+        this.action = 'Voltar';
+        break;
+      default:
+        this.path = 'current';
+        this.action = 'Clima agora';
+        break;
+    }
+  }
+}
