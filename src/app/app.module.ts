91f7c7313cc5d7d208a6340c2c2e2d2dc8fc2900
adlermo <adlerorteney@gmail.com>
2022-02-24 23:07:02 -0300

    Changing httpClient import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
@@ -15,8 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     CoreModule,
     FormsModule,
-    HttpClientModule,
-    ReactiveFormsModule,
     SharedModule,
   ],
   declarations: [AppComponent, HomeComponent],
